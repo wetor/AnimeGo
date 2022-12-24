ed9d76c25505eab9d4be696159043a0df3960173
wetor <wetorx@qq.com>
2022-12-24 17:03:44 +0800

    v0.6.0 feature-gpython - 修复raw_parser.py以支持gpython运行

--- internal/plugin/python/script/raw_parser.py
+++ internal/plugin/python/script/raw_parser.py
@@ -1,10 +1,6 @@
-import logging
 import re
-from dataclasses import dataclass
 
 # from parser.episode import Episode
-
-logger = logging.getLogger(__name__)
 
 EPISODE_RE = re.compile(r"\d+")
 TITLE_RE = re.compile(
@@ -28,31 +24,37 @@ CHINESE_NUMBER_MAP = {
 }
 
 
-@dataclass
 class Episode:
-    title_en: str or None
-    title_zh: str or None
-    title_jp: str or None
-    season: int
-    season_raw: str
-    episode: int
-    sub: str
-    group: str
-    resolution: str
-    source: str
 
+    def __init__(self, name_en, name_zh, name_jp, season, sr, episode, sub, group, dpi, source):
+        self.title_en = name_en
+        self.title_zh = name_zh
+        self.title_jp = name_jp
+        self.season = season
+        self.season_raw = sr
+        self.episode = episode
+        self.sub = sub
+        self.group = group
+        self.resolution = dpi
+        self.source = source
+
+    def __str__(self):
+        return "title_en:「%s」, title_zh:「%s」, title_jp:「%s」, season: %s, season_raw: %s, episode: %s, " \
+               "sub: %s, resolution: %s, source: %s, group: %s" % \
+               (self.title_en, self.title_zh, self.title_jp, self.season, self.season_raw,
+                self.episode, self.sub, self.resolution, self.source, self.group)
 
 class RawParser:
     @staticmethod
-    def get_group(name: str) -> str:
+    def get_group(name):
         return re.split(r"[\[\]]", name)[1]
 
     @staticmethod
-    def pre_process(raw_name: str) -> str:
+    def pre_process(raw_name):
         return raw_name.replace("【", "[").replace("】", "]")
 
     @staticmethod
-    def season_process(season_info: str):
+    def season_process(season_info):
         if re.search(r"新番|月?番", season_info):
             name_season = re.sub(".*新番.", "", season_info)
         else:
@@ -78,8 +80,8 @@ class RawParser:
         return name, season_raw, season
 
     @staticmethod
-    def name_process(name: str):
-        name_en, name_zh, name_jp = None, None, None
+    def name_process(name):
+        name_en, name_zh, name_jp = "", "", ""
         name = name.strip()
         split = re.split("/|\s{2}|-\s{2}", name.replace("（仅限港澳台地区）", ""))
         while "" in split:
@@ -92,14 +94,14 @@ class RawParser:
         if len(split) == 1:
             split_space = name.split(" ")
             for idx, item in enumerate(split_space):
-                if re.search(r"^[\u4e00-\u9fa5]{2,}", item) is not None:
+                if re.search("^[\u4e00-\u9fa5]{2,}", item) is not None:
                     split_space.remove(item)
                     split = [item.strip(), " ".join(split_space).strip()]
                     break
         for item in split:
-            if re.search(r"[\u0800-\u4e00]{2,}", item) and not name_jp:
+            if re.search("[\u0800-\u4e00]{2,}", item) and not name_jp:
                 name_jp = item.strip()
-            elif re.search(r"[\u4e00-\u9fa5]{2,}", item) and not name_zh:
+            elif re.search("[\u4e00-\u9fa5]{2,}", item) and not name_zh:
                 name_zh = item.strip()
             elif re.search(r"[a-zA-Z]{3,}", item) and not name_en:
                 name_en = item.strip()
@@ -109,7 +111,7 @@ class RawParser:
     def find_tags(other):
         elements = re.sub(r"[\[\]()（）]", " ", other).split(" ")
         # find CHT
-        sub, resolution, source = None, None, None
+        sub, resolution, source = "", "", ""
         for element in filter(lambda x: x != "", elements):
             if SUB_RE.search(element):
                 sub = element
@@ -117,15 +119,11 @@ class RawParser:
                 resolution = element
             elif SOURCE_RE.search(element):
                 source = element
-        return RawParser.clean_sub(sub), resolution, source
+        if sub is not None:
+            sub = re.sub(r"_MP4|_MKV", "", sub)
+        return sub, resolution, source
 
-    @staticmethod
-    def clean_sub(sub: str | None) -> str | None:
-        if sub is None:
-            return sub
-        return re.sub(r"_MP4|_MKV", "", sub)
-
-    def process(self, raw_title: str):
+    def process(self, raw_title):
         raw_title = raw_title.strip()
         content_title = self.pre_process(raw_title)  # 预处理标题
         group = self.get_group(content_title)  # 翻译组的名字
@@ -147,21 +145,20 @@ class RawParser:
         sub, dpi, source = self.find_tags(other)  # 剩余信息处理
         return name_en, name_zh, name_jp, season, season_raw, episode, sub, dpi, source, group
 
-    def analyse(self, raw: str) -> Episode or None:
+    def analyse(self, raw):
         try:
             ret = self.process(raw)
             if ret is None:
                 return None
-            name_en, name_zh, name_jp, season, sr, episode, \
-            sub, dpi, source, group = ret
+            name_en, name_zh, name_jp, season, sr, episode, sub, dpi, source, group = ret
         except Exception as e:
-            logger.error(f"Parser cannot analyse {raw} {e}")
+            print('Parser cannot analyse', raw, e)
             return None
         return Episode(name_en, name_zh, name_jp, season, sr, episode, sub, group, dpi, source)
 
 
-if __name__ == "__main__":
-    test = RawParser()
-    test_txt = "[梦蓝字幕组]New Doraemon 哆啦A梦新番[716][2022.07.23][AVC][10080P][GB_JP]"
-    ep = test.analyse(test_txt)
-    print(f"en:{ep.title_en}, zh:{ep.title_zh}, jp:{ep.title_jp}, group:{ep.group}")
+test = RawParser()
+test_txt = "[梦蓝字幕组]New Doraemon 哆啦A梦新番[716][2022.07.23][AVC][10080P][GB_JP]"
+ep = test.analyse(test_txt)
+print(ep)
+print("en:%s, zh:%s, jp:%s, group:%s" % (ep.title_en, ep.title_zh, ep.title_jp, ep.group))
